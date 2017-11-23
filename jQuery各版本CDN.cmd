8ebd1a29040077e550d76672f904e61034802a1f
Charlie <2531763117@qq.com>
2017-11-23 09:28:42 +0800

    Create jQuery各版本CDN.cmd

---
+++ jQuery各版本CDN.cmd
@@ -0,0 +1,96 @@
+jquery-2.1.1
+
+ 
+
+注:jquery-2.0以上版本不再支持IE 6/7/8)百度引用地址 (推荐目前最稳定的，不会出现延时打不开情况)
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/2.1.1/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-2.1.1.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-2.1.1.min.js”></script>
+
+
+jquery-2.0.0
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/2.0.0/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-2.0.0.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-2.0.0.min.js”></script>
+
+ 
+
+jquery-1.11.1
+
+ 
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.11.1/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.11.1.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.11.1.min.js”></script>
+
+ 
+
+jquery-1.10.2
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.10.2/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.10.2.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.10.2.min.js”></script>
+
+
+jquery-1.9.1
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.9.1/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.9.1.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.9.1.min.js”></script>
+
+
+jquery-1.8.3
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.8.3/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.8.3.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.8.3.min.js”></script>
+
+ 
+
+jquery-1.7.2
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.7.2/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.2.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.7.2.min.js”></script>
+
+ 
+
+
+jquery-1.6.4
+
+
+百度压缩版引用地址:
+<script src=”http://libs.baidu.com/jquery/1.6.4/jquery.min.js”></script>
+微软压缩版引用地址:
+<script src=”http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.6.4.min.js”></script>
+官网jquery压缩版引用地址:
+<script src=”http://code.jquery.com/jquery-1.6.4.min.js”></script>
